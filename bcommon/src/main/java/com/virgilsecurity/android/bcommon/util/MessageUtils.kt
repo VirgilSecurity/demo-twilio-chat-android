160dd6590dd5bd01dd76fe9c0559437b44ad830b
cardoso <matheus@cardo.so>
2020-03-25 18:08:18 -0300

    Fix crash when receiving malformed message

--- bcommon/src/main/java/com/virgilsecurity/android/bcommon/util/MessageUtils.kt
+++ bcommon/src/main/java/com/virgilsecurity/android/bcommon/util/MessageUtils.kt
@@ -21,27 +21,31 @@ class MessageUtils {
                 return text
             }
 
-            val map = JsonUtils.stringToMap(text)
+            try {
+                val map = JsonUtils.stringToMap(text)
 
-            when (message.version) {
-                in "v1" -> return text
-                in "v2" -> {
-                    when (val type = map["type"] as String) {
-                        in "text" -> {
-                            val payload = map["payload"] as? Map<*, *>
-                                    ?: return "[Message does not include payload]"
+                when (message.version) {
+                    in "v1" -> return text
+                    in "v2" -> {
+                        when (val type = map["type"] as String) {
+                            in "text" -> {
+                                val payload = map["payload"] as? Map<*, *>
+                                        ?: return "[Message does not include payload]"
 
-                            return payload["body"] as? String
-                                    ?: "[Message does not include payload.body]"
-                        }
-                        else -> {
-                            return "[Message type '$type' is not yet supported]"
+                                return payload["body"] as? String
+                                        ?: "[Message does not include payload.body]"
+                            }
+                            else -> {
+                                return "[Message type '$type' is not yet supported]"
+                            }
                         }
                     }
+                    else -> {
+                        return "[Update your app to see this message]"
+                    }
                 }
-                else -> {
-                    return "[Update your app to see this message]"
-                }
+            } catch (e: Exception) {
+                return "[Could not decode this message: $text. Error: $e]"
             }
         }
 
